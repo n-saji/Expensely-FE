5e9947cc777f60bfd81200044cd7f6c3801b1ccc
Nikhil Saji <nikhilsaji200@gmail.com>
2025-12-07 11:15:10 -0500

    Added overall website maintainence mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,10 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const maintenanceMode = process.env.NEXT_MAINTENANCE_MODE === "true";
 
-
 export const metadata: Metadata = {
-  title: maintenanceMode ? "Under Maintenance | Expensely" : "Expensely - Track and Manage Your Expenses",
+  title: maintenanceMode
+    ? "Under Maintenance | Expensely"
+    : "Expensely - Track and Manage Your Expenses",
   description:
     "Expensely helps you manage budgets, track spending, and visualize your finances securely.",
 };
@@ -22,7 +23,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`antialiased bg-background `}>
-        <ReduxProvider>{children}</ReduxProvider>
+        <ReduxProvider>
+          {maintenanceMode && (
+            <div className="min-h-screen flex flex-col items-center justify-center bg-background text-center px-4">
+              <h1 className="text-3xl font-semibold mb-2">
+                We’ll be back soon!
+              </h1>
+              <p className="text-gray-600 text-lg">
+                Our site is currently undergoing scheduled maintenance.
+                <br />
+                Thank you for your patience.
+              </p>
+            </div>
+          )}
+          {!maintenanceMode && children}
+        </ReduxProvider>
         <Analytics />
         <SpeedInsights />
       </body>
