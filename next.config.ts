bb2c3a9260f1769ba3fc98c1f88eab149d33a9c2
Nikhil Saji <nikhilsaji200@gmail.com>
2025-12-09 18:18:43 -0500

    added local failing issue fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-images: {
-    domains: ['qidkhnousdwexgmgvytk.supabase.co'],
+  images: {
+    domains: ["qidkhnousdwexgmgvytk.supabase.co"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // all /api calls
+        destination: process.env.NEXT_PUBLIC_API_URL + "/:path*", // redirect to Spring Boot locally
+      },
+    ];
   },
 };
 
